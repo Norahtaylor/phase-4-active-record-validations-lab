f9b454897dfd805652a18a2014e9070fe31a6a9c
[Norah Taylor] <emt368@gmail.com>
2022-06-29 18:01:44 -0400

    commit tree c6a98cafccbeadac87a1c1fd8434415bfd0e9e4c

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,10 @@
 class Author < ApplicationRecord
+    validates :name, presence: true 
+    validates :name.downcase, uniqueness: true 
+
+    #could combine the above to 
+    #validates :name, presence: true, uniqueness: true
+
+    validates :phone_number, length: {is: 10}
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+    validates :title, presence: true 
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ['Fiction', 'Non-Fiction'], message: "Category must be Fiction or Non-Fiction"}
+
+    validate :must_have_title
+
+    def must_have_title
+        unless title.match?("Won't Believe", "Secret", "Top [number]", "Guess")
+            errors.add(:title, "Title must be clickbait-y")
+        end 
+    end 
+
+    # def clickbait?
+    #     if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+    #       errors.add(:title, "must be clickbait")
+    #     end
+    #   end
 end
